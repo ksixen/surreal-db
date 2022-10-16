69d861634cdf6a63d5608d4e2d852c0be161f18e
Altaev <altaev@clout.one>
2022-10-16 17:35:20 +0300

    setup localdb

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 import express from "express";
-import { getUserById, getUsersList } from "./reqFuncs/person.js";
+import {
+  editUser,
+  getUserById,
+  getUsersList,
+  loginUser,
+} from "./reqFuncs/person.js";
 import { surrealDB } from "./surreal.js";
-// const Surreal = require('');
 
 const PORT = 4000;
 const app = express();
 
 app.use(express.json());
-
-app.get("/getUser", getUserById);
+app.get("/getUserById", getUserById);
 app.get("/getUsersList", getUsersList);
+app.get("/editUser", editUser);
+app.post("/login", loginUser);
+
+const createTestUser = () => {
+  surrealDB.createTable({
+    table: "person",
+    props: {
+      name: `surreal-user${Math.random().toString(7)}`,
+      login: Math.random().toString(7),
+      password: Math.random().toString(7),
+    },
+  });
+};
 
 app.listen(PORT, () => {
   surrealDB.initDB().then(() => {
+    createTestUser()
     console.log("ServerStartedOnPort", `http://localhost:${PORT}`);
   });
 });
